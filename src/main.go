606f2d70658cbe0316bfa3c5a1d9efbe88355819
Mathias Erik Nord <mathias.erik.nord@gmail.com>
2026-02-23 14:34:57 +0100

    Added exchange endpoint.

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"assignment_one/src/exchange"
 	"assignment_one/src/info"
 	"assignment_one/src/status"
 	"log"
@@ -12,12 +13,12 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		// Log error if .env file is not found, but don't exit if it's optional
 		log.Println("Error loading .env file, assuming production environment variables are set")
 	}
 
 	http.Handle("/v1/status", http.HandlerFunc(status.GetStatus))
 	http.Handle("/v1/info/", http.HandlerFunc(info.GetInfo))
+	http.Handle("/v1/exchange/", http.HandlerFunc(exchange.GetExchange))
 
 	log.Println("Service is up and running.")
 	log.Fatal(http.ListenAndServe(":8080", nil))
