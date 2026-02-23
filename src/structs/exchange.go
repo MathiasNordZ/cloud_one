ef41ffbd4622f20936e0c492cd25aebcdd0bf89d
Mathias Erik Nord <mathias.erik.nord@gmail.com>
2026-02-23 14:33:33 +0100

    Created helper structs.

--- src/structs/exchange.go
+++ src/structs/exchange.go
@@ -10,10 +10,21 @@ type CountryAPI struct {
 	Name struct {
 		Common string `json:"common"`
 	} `json:"name"`
-	Borders []string `json:"borders"`
+	Borders    []string `json:"borders"`
+	Currencies map[string]struct {
+		Name   string `json:"name"`
+		Symbol string `json:"symbol"`
+	} `json:"currencies"`
 }
 
 type CurrencyAPI struct {
 	BaseCode string             `json:"base_code"`
 	Rates    map[string]float64 `json:"rates"`
+}
+
+type CountryCurrencyResponse []struct {
+	CCA2       string `json:"cca2"`
+	Currencies map[string]struct {
+		Name string `json:"name"`
+	} `json:"currencies"`
 }
